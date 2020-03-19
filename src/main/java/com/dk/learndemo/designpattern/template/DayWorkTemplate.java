e002d6b618e584e5273374dad57533af5d51d404
zhudakang <zhudakang@situdata.com>
2020-03-19 15:44:42 +0800

    MOD:增加注释

--- src/main/java/com/dk/learndemo/designpattern/template/DayWorkTemplate.java
+++ src/main/java/com/dk/learndemo/designpattern/template/DayWorkTemplate.java
@@ -8,6 +8,8 @@ package com.dk.learndemo.designpattern.template;
  *                 比如核验身份证信息
  *                 1.上传图片 2. 调用数据源 3. 得到返回结果存储数据库并包装返回
  *                 那么也是一种模板模式，一个总体的方法，我们分别实现其中的内容。
+ *                 比如父类中的上传图片，大家都是一样的，那么就没必要写多份了。
+ *                 那么父类中的上传图片方法不是抽象的，可以直接用，那么每一家都可以只用实现那必要的数据源部分了~
  * @create : 2020/03/19
  */
 public abstract class DayWorkTemplate {
