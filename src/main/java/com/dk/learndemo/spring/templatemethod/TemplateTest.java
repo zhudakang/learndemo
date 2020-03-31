1a128f6118588921338cf85f3cf179dd60cc8260
zhudakang <zhudakang@situdata.com>
2020-03-31 14:31:11 +0800

    MOD:增加eureka笔记

--- src/main/java/com/dk/learndemo/spring/templatemethod/TemplateTest.java
+++ src/main/java/com/dk/learndemo/spring/templatemethod/TemplateTest.java
@@ -3,6 +3,10 @@ package com.dk.learndemo.spring.templatemethod;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.util.Pool;
 
 /**
  * @author :zhudakang
@@ -20,4 +24,24 @@ public class TemplateTest {
      * */
     @Autowired
     AmqpTemplate amqpTemplate;
+
+    /**
+     * 原始的jedis实现
+     * 但是springboot只要实现一个RedisConfig和对应的Redis配置就好
+     * */
+    public void test(){
+        // 1.封装config
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxIdle(20);
+        poolConfig.setMaxTotal(200);
+        poolConfig.setMaxWaitMillis(2000);
+        // 2.创建jedis池
+        Pool<Jedis> pools = new JedisPool(poolConfig,"localhost",6379,2000,null,false);
+        // 3.获取jedis
+        Jedis jedis = pools.getResource();
+        // 4.jedis操作
+        jedis.set("name".getBytes(), "lucy".getBytes());
+        boolean exists = jedis.exists("name");
+        System.out.println(exists);
+    }
 }
