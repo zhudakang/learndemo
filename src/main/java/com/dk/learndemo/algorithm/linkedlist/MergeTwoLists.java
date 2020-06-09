68aeef9d08d197fc0d3a2a1993880b4c20831270
zhudakang <zhudakang@situdata.com>
2020-06-09 19:59:51 +0800

    MOD:合并两个升序链表

--- src/main/java/com/dk/learndemo/algorithm/linkedlist/MergeTwoLists.java
+++ src/main/java/com/dk/learndemo/algorithm/linkedlist/MergeTwoLists.java
@@ -17,6 +17,8 @@ public class MergeTwoLists {
      * DK解法和思路：把两个都放到list中，然后进行升序，最后输出
      * 输入：1->2->4, 1->3->4
      * 输出：1->1->2->3->4->4
+     *
+     * 不好意思，我没看是升序链表= =
      */
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) {
@@ -48,6 +50,49 @@ public class MergeTwoLists {
             other = other.next;
         }
         return head;
+    }
+
+    /**
+     * 官方题解，惊呼nb
+     * */
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists2(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists2(l1, l2.next);
+            return l2;
+        }
+
+    }
+
+    /**
+     * 这个是改变了入参的一种做法
+     * */
+    public ListNode mergeTwoLists3(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 == null ? l2 : l1;
+        return prehead.next;
     }
 
     public static void main(String[] args) {
