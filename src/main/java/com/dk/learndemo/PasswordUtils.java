39a7851138f34017f8e0833e19e3b0d258edf1bc
zhudakang <zhudakang@situdata.com>
2020-06-10 17:38:02 +0800

    环形替换法

--- src/main/java/com/dk/learndemo/PasswordUtils.java
+++ src/main/java/com/dk/learndemo/PasswordUtils.java
@@ -21,6 +21,6 @@ public class PasswordUtils {
     }
 
     public static void main(String[] args) {
-        System.out.println(encodePassword("bnzj4490"));
+        System.out.println(encodePassword("12345678"));
     }
 }
--- src/main/java/com/dk/learndemo/algorithm/arr/Rotate.java
+++ src/main/java/com/dk/learndemo/algorithm/arr/Rotate.java
@@ -75,6 +75,7 @@ public class Rotate {
      * 大呼 nb
      */
     public void rotate3(int[] nums, int k) {
+        //因为K有可能是长度的几倍，所以取余
         k %= nums.length;
         reverse(nums, 0, nums.length - 1);
         reverse(nums, 0, k - 1);
@@ -94,6 +95,12 @@ public class Rotate {
     /**
      * 环形替换法：
      * 最牛逼的解法
+     * 换桌思路好评，但是还挺难理解的
+     * 总体的思想就是让他换k个位置，被占了之后，让那个人自己再去往后面移动K个位置
+     * 把元素看做同学，把下标看做座位，大家换座位。第一个同学离开座位去第k+1个座位，第k+1个座位的同学被挤出去了，
+     * 他就去坐他后k个座位，如此反复。但是会出现一种情况，就是其中一个同学被挤开之后，
+     * 坐到了第一个同学的位置（空位置，没人被挤出来），但是此时还有人没有调换位置，这样就顺着让第二个同学换位置。
+     * 那么什么时候就可以保证每个同学都换完了呢？n个同学，换n次，所以用一个count来计数即可。
      * */
     public void rotate4(int[] nums, int k) {
         k = k % nums.length;
@@ -106,9 +113,10 @@ public class Rotate {
                 int temp = nums[next];
                 nums[next] = prev;
                 prev = temp;
+                //从下一个作为开始坐下来
                 current = next;
                 count++;
-            } while (start != current);
+            } while (start != current); // 说明做的没有空位了
         }
     }
 }
