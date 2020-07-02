6455f1c48e0e4efe3f013ebfcad19b16f5dd804a
zhudakang <zhudakang@situdata.com>
2020-07-02 20:34:57 +0800

    排序数组找到目标值  二分查找变种

---
+++ src/main/java/com/dk/learndemo/algorithm/yyyuyin/SearchInsert.java
@@ -0,0 +1,45 @@
+package com.dk.learndemo.algorithm.yyyuyin;
+
+/**
+ * @Author : zhudakang@situdata.com
+ * @Description :
+ * @Date : 2020/7/2
+ */
+public class SearchInsert {
+
+    /**
+     * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+     *
+     * 你可以假设数组中无重复元素。
+     *
+     * 示例 1:
+     *
+     * 输入: [1,3,5,6], 5
+     * 输出: 2
+     * */
+    public int searchInsert(int[] nums, int target) {
+
+        int len = nums.length;
+        if (len == 0) {
+            return 0;
+        }
+
+        // 特判
+        if (nums[len - 1] < target) {
+            return len;
+        }
+        int left = 0;
+        int right = len - 1;
+        while (left < right) {
+            int mid = (left + right) >>> 1;
+            // 严格小于 target 的元素一定不是解
+            if (nums[mid] < target) {
+                // 下一轮搜索区间是 [mid + 1, right]
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
