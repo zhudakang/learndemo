22dde81ab1dd81dab7b176e53886dc0c6e03838d
zhudakang <zhudakang@situdata.com>
2020-07-03 11:59:43 +0800

    链表相加

---
+++ src/main/java/com/dk/learndemo/algorithm/yyyuyin/ListNode.java
@@ -0,0 +1,15 @@
+package com.dk.learndemo.algorithm.yyyuyin;
+
+/**
+ * @author :zhudakang
+ * @description : ListNode
+ * @create : 2020/07/03
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
