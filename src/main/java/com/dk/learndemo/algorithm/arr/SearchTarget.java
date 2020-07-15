c1095e9956d77ed88e73d21d965e23248c15257c
zhudakang <zhudakang@situdata.com>
2020-07-15 14:24:29 +0800

    二分查找的变种应用

--- src/main/java/com/dk/learndemo/algorithm/arr/SearchTarget.java
+++ src/main/java/com/dk/learndemo/algorithm/arr/SearchTarget.java
@@ -43,6 +43,47 @@ public class SearchTarget {
     }
 
     public static void main(String[] args) {
-        search(new int[]{5, 7, 7, 8, 8, 10}, 8);
+        System.out.println(searchRange(new int[]{5, 7, 7, 8,  10}, 8));
+    }
+
+    //思路很简单，但是,需要处理边界值
+    /**
+     * 与上一题不太一样，
+     * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+     *
+     * 你的算法时间复杂度必须是 O(log n) 级别。
+     */
+    private static int extremeInsertionIndex(int[] nums, int target, boolean left) {
+        int lo = 0;
+        int hi = nums.length;
+
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] > target || (left && target == nums[mid])) {
+                hi = mid;
+            }
+            else {
+                lo = mid+1;
+            }
+        }
+
+        return lo;
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        int[] targetRange = {-1, -1};
+
+        int leftIdx = extremeInsertionIndex(nums, target, true);
+
+        // assert that `leftIdx` is within the array bounds and that `target`
+        // is actually in `nums`.
+        if (leftIdx == nums.length || nums[leftIdx] != target) {
+            return targetRange;
+        }
+
+        targetRange[0] = leftIdx;
+        targetRange[1] = extremeInsertionIndex(nums, target, false)-1;
+
+        return targetRange;
     }
 }
