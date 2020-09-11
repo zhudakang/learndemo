b262f37f57e73e82655296680006e80153cf1ca6
zhudakang <zhudakang@baijiahulian.com>
2020-09-11 14:53:55 +0800

    求list交集

--- src/main/java/com/dk/learndemo/A.java
+++ src/main/java/com/dk/learndemo/A.java
@@ -1,5 +1,9 @@
 package com.dk.learndemo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * @author :zhudakang
  * @description : A
@@ -11,4 +15,36 @@ public class A {
         System.out.println("A");
         System.out.println();
     }
+
+    public static void main(String[] args) {
+        List<Long> list = new ArrayList<>();
+        list.add(1L);
+        list.add(2L);
+        list.add(8L);
+        list.add(7L);
+        list.add(0L);
+
+        List<Long> list2 = new ArrayList<>();
+        list2.add(1L);
+        list2.add(8L);
+        list2.add(7L);
+        list2.add(0L);
+        List<List<Long>> test = new ArrayList<>();
+        test.add(list);
+        test.add(list2);
+        List<Long> a = retainElementList(test);
+
+    }
+
+    public static List<Long> retainElementList(List<List<Long>> elementLists) {
+
+        Optional<List<Long>> result = elementLists.parallelStream()
+                .filter(elementList -> elementList != null && ((List) elementList).size() != 0)
+                .reduce((a, b) -> {
+                    a.retainAll(b);
+                    return a;
+                });
+        return result.orElse(new ArrayList<>());
+    }
+
 }
